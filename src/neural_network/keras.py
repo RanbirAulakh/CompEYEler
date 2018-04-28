8cc7c28cd729f9f33d4174b25c8781b4fda68a3d
DatKami <studiowolf8@gmail.com>
2018-04-28 11:07:05 -0400

    Now decodes spaces properly

--- src/neural_network/keras.py
+++ src/neural_network/keras.py
@@ -79,7 +79,7 @@ model.add(Flatten())
 model.add(Dense(500, activation="relu"))
 
 # Output layer with 70 nodes (one for each possible letter/number we predict)
-model.add(Dense(70, activation="softmax"))
+model.add(Dense(71, activation="softmax"))
 
 # Ask Keras to build the TensorFlow model behind the scenes
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
