d1fd05489e156fa26cbb2fffce733380bef0619a
Joe Bartelmo <joebartelmo@gmail.com>
2018-04-28 12:20:55 -0400

    Add BRandon's work  and requirements

--- src/neural_network/main.py
+++ src/neural_network/main.py
@@ -1,44 +1,22 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 from classifier import get_data
+from neural_networks import cnn_model_fn
+import keras
 
-# Imports
-import tensorflow as tf
+def main():
+    images, labels = get_data()
+    model = cnn_model_fn(1000)
 
-tf.logging.set_verbosity(tf.logging.INFO)
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.SGD(lr=0.01),
+                  metrics=['accuracy'])
 
-def main(unused_argv):
-    with tf.Session() as sess:
-        # Get Image data and labels
-        images, characters = get_data()
-
-        # Initialize placeholders
-        x = tf.placeholder(dtype=tf.float32, shape=[None, 20, 20])
-        y = tf.placeholder(dtype=tf.int32, shape=[None])
-
-        # Flatten the input data
-        images_flat = tf.contrib.layers.flatten(x)
-
-        # Fully connected layer
-        logits = tf.contrib.layers.fully_connected(images_flat, 62, tf.nn.relu)
-
-        # Define a loss function
-        loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,
-                                                                             logits=logits))
-        # Define an optimizer
-        train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
-
-        # Convert logits to label indexes
-        correct_pred = tf.argmax(logits, 1)
-
-        # Define an accuracy metric
-        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-        for i in range(201):
-            _, loss_value = sess.run([train_op, loss], feed_dict={x: images, y: characters})
-            if i % 10 == 0:
-                print("Loss: ", loss)
-
+    #train
+    model.fit(images, labels,
+              batch_size=batch_size,
+              epochs=100,
+              verbose=1,
+              validation_data=(x_test, y_test),
+              callbacks=[history])
 
 if __name__ == "__main__":
-      tf.app.run()
+      main()
